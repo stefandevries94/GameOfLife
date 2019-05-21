e0cc47fa471c75b0f674617b5ed25cdfc2873354
stefandevries94 <stefandevries94@gmail.com>
2019-05-21 21:02:17 +0200

    Tkinter simulation is now fully working
    Issue caused the simulation to get slower and slower due to the creation of many rectangles. Changed the code to only create rectangles once and alter the color based on the rules

--- Second_file.py
+++ Second_file.py
@@ -1,16 +1,20 @@
 import tkinter as tk
 import random
 
-num_col = 20
-num_row = 20
+
+
 
 class App(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         self.canvas = tk.Canvas(self, width=500, height=500, borderwidth=0, highlightthickness=0)
         self.canvas.pack(side="top", fill="both", expand="true")
-        self.cellwidth = 25
-        self.cellheight = 25
+        self.cellwidth = 10
+        self.cellheight = 10
+        self.num_col = int(500 / self.cellwidth)
+        self.num_row =int(500 / self.cellheight)
+
+
 
         self.clear_screen()
 
@@ -18,9 +22,9 @@ class App(tk.Tk):
         self.grids = []
         self.init_grids()
         self.set_grid()
+        self.draw_canvas()
 
-
-        self.redraw(500)
+        self.redraw(100)
 
 
 
@@ -33,8 +37,8 @@ class App(tk.Tk):
 
         def create_grid():
             rows = []
-            for row_num in range(num_row):
-                list_of_columns = [0] * num_col
+            for row_num in range(self.num_row):
+                list_of_columns = [0] * self.num_col
                 rows.append(list_of_columns)
             return rows
 
@@ -42,8 +46,8 @@ class App(tk.Tk):
         self.grids.append(create_grid())
 
     def set_grid(self, value=None):
-        for r in range(num_row):
-            for c in range(num_col):
+        for r in range(self.num_row):
+            for c in range(self.num_col):
                 if value is None:
                     cell_value = random.choice([0, 1])
                 else:
@@ -52,9 +56,9 @@ class App(tk.Tk):
 
     def draw_canvas(self):
         self.clear_screen()
-        self.rect = {}
-        for row in range(num_row):
-            for column in range(num_col):
+
+        for row in range(self.num_row):
+            for column in range(self.num_col):
                 x1 = column*self.cellwidth
                 y1 = row * self.cellheight
                 x2 = x1 + self.cellwidth
@@ -63,17 +67,20 @@ class App(tk.Tk):
                     color = "black"
                 else:
                     color = "white"
-                self.rect[row,column] = self.canvas.create_rectangle(x1,y1,x2,y2, fill=color, tags="rect")
+                self.rect[(row,column)] = self.canvas.create_rectangle(x1,y1,x2,y2, fill=color, tags="rect")
 
     def clear_screen(self):
-        self.rect = {}
-        for row in range(num_row):
-            for column in range(num_col):
-                x1 = column * self.cellwidth
-                y1 = row * self.cellheight
-                x2 = x1 + self.cellwidth
-                y2 = y1 + self.cellheight
-                self.rect[row, column] = self.canvas.create_rectangle(x1, y1, x2, y2, fill="white", tags="rect")
+        self.rect = dict()
+        for row in range(self.num_row):
+            for column in range(self.num_col):
+                self.x1 = column * self.cellwidth
+                self.y1 = row * self.cellheight
+                self.x2 = self.x1 + self.cellwidth
+                self.y2 = self.y1 + self.cellheight
+                self.rect[(row, column)] = self.canvas.create_rectangle(self.x1, self.y1, self.x2, self.y2, fill="white", tags="rect")
+
+
+
 
     def get_cell(self, r, c):
         try:
@@ -81,6 +88,8 @@ class App(tk.Tk):
         except:
             cell_value = 0
         return cell_value
+
+
 
     def check_neighbors(self, row_index, col_index):
         num_alive_neighbors = 0
@@ -110,8 +119,8 @@ class App(tk.Tk):
 
     def update_generation(self):
         # self.set_grid(0)
-        for r in range(num_row):
-            for c in range(num_col):
+        for r in range(self.num_row):
+            for c in range(self.num_col):
                 next_gen_state = self.check_neighbors(r, c)
                 self.grids[self.inactive_grid()][r][c] = next_gen_state
         self.active_grid = self.inactive_grid()
@@ -120,14 +129,28 @@ class App(tk.Tk):
         return (self.active_grid + 1) % 2
 
     def redraw(self, delay):
-        self.update_generation()
-        self.draw_canvas()
-        self.after(delay, lambda: self.redraw(delay))
 
-    def run(self):
+        # self.canvas.itemconfig("rect", fill="white")
+
+        # self.draw_canvas()
+
+
+        for r in range(self.num_row):
+            for c in range(self.num_col):
+                if self.grids[self.active_grid][r][c] == 1:
+                    color = "black"
+
+                else:
+                    color = "white"
+                self.canvas.itemconfig(self.rect[(r,c)], fill=color)
+
+        self.after(delay, lambda: self.redraw(delay))
         self.update_generation()
-        self.draw_canvas()
-        self.redraw(1000)
+
+
+
+
+
 
 if __name__ == "__main__":
     app = App()
